bf0615d504c9f4bc7b28a3eb107cfcc1efba87e6
lx <lixiang_uestc@126.com>
2020-09-14 23:34:33 +0800

    多个子测试

--- hello/hello.go
+++ hello/hello.go
@@ -4,11 +4,26 @@ import (
 	"fmt"
 )
 
+const spanish = "Spanish"
+const french = "French"
+const englishHelloPrefix = "Hello,"
+const spanishHelloPrefix = "Hola,"
+const frenchHelloPrefix = "Bonjour,"
+
 //Hello :return Hello World.
-func Hello(name string) string {
-	return "Hello " + name
+func Hello(name, language string) string {
+	if name == "" {
+		name = "world"
+	}
+	if language == spanish {
+		return spanishHelloPrefix + name
+	}
+	if language == french{
+		return frenchHelloPrefix+name
+	}
+	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("World"))
+	fmt.Println(Hello("World", ""))
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,38 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	want := "Hello girl"
-	got := Hello("girl")
-
-	if got != want {
-		t.Errorf("\n got '%q' \n want '%q'", got, want)
+	assertCorrentMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("want '%q',got '%q'", want, got)
+		}
 	}
+
+	t.Run("say hello to people", func(t *testing.T) {
+		want := "Hello,girl"
+		got := Hello("girl","")
+
+		assertCorrentMessage(t, got, want)
+	})
+
+	t.Run("say hello world when a empty name is supplied", func(t *testing.T) {
+		want := "Hello,world"
+		got := Hello("","")
+
+		assertCorrentMessage(t, got, want)
+	})
+
+	t.Run("in spanish",func(t *testing.T){
+		want:="Hola,Elodie"
+		got:=Hello("Elodie","Spanish")
+
+		assertCorrentMessage(t,got,want)
+	})
+
+	t.Run("in french",func(t *testing.T){
+		want:="Bonjour,alph"
+		got:=Hello("alph","French")
+
+		assertCorrentMessage(t,got,want)
+	})
 }
