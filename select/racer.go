d325eec90fb891fdc1aade526ad020d15533e0ea
lx <lixiang_uestc@126.com>
2020-09-28 21:16:43 +0800

    add select

---
+++ select/racer.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+	"time"
+)
+
+func Racer(a, b string) string {
+	startA := time.Now()
+	http.Get(a)
+	durationA := time.Since(startA)
+
+	startB := time.Now()
+	http.Get(b)
+	durationB := time.Since(startB)
+
+	if durationA>durationB{
+		return b
+	}
+	return a
+}
